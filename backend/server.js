c8a8ed4d5953a3daa6edf6c46658d35b7bcd84a4
Sachin18022006 <sachinbssachinbs18@gmail.com>
2026-03-28 14:56:15 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,11 @@ const authRoutes = require("./routes/authRoutes");
 require("dotenv").config();
 
 const app = express();
+const cors = require("cors");
+
+app.use(cors({
+  origin: "*"
+}));
 
 app.use(cors());
 app.use(express.json());
