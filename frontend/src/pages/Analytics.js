31dbc0eb22fb3cd066569bfbb6d4c32139808d0d
Sachin18022006 <sachinbssachinbs18@gmail.com>
2026-03-29 10:48:56 +0530

    changes

--- frontend/src/pages/Analytics.js
+++ frontend/src/pages/Analytics.js
@@ -106,7 +106,7 @@ function Analytics({ month, setMonth }) {
           {/* PIE */}
           <div className="chart-card">
             <h3>Category Split</h3>
-            <PieChart width={250} height={250}>
+            <PieChart width={300} height={250}>
               <Pie data={data} dataKey="amount" outerRadius={90}>
                 {data.map((_, i) => (
                   <Cell key={i} fill={COLORS[i % COLORS.length]} />
